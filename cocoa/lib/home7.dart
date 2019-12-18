71f6fa5a0178aef19f98c08c83b9e38341f28686
Apple <apple@AppledeMacBook-Pro.local>
2019-12-18 17:36:35 +0800

    cell

--- cocoa/lib/home7.dart
+++ cocoa/lib/home7.dart
@@ -1,25 +1,22 @@
 import 'package:flutter/material.dart';
 
 class HomePage7 extends StatefulWidget {
-  HomePage7({Key key, this.arguments}) : super(key: key);
   final Map arguments;
+  HomePage7({Key key, this.arguments}) : super(key: key);
 
   @override
-  _HomePage7State createState() => _HomePage7State();
+  _HomePage7State createState() => _HomePage7State(arguments: this.arguments);
 }
 
 class _HomePage7State extends State<HomePage7> {
-Map arguments;
-@override
-void initState() { 
-  super.initState();
-  arguments = widget.arguments;
-}
+  Map arguments;
+  _HomePage7State({this.arguments});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('home7 id ${arguments}'),
+        title: Text('home7 id ${this.arguments}'),
       ),
       body: Container(
         child: Text('content'),
